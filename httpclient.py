de7d64a0ac18ca6a573898b483f5f6d2d8804971
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-10-10 15:20:39 -0600

    init commit

--- httpclient.py
+++ httpclient.py
@@ -31,6 +31,9 @@ class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
+    
+    def __repr__(self) -> str:
+        return self.code +'\n' + self.body
 
 class HTTPClient(object):
     #def get_host_port(self,url):
@@ -41,13 +44,13 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        return int(data.split("\r\n")[0].split(" ")[1])
 
     def get_headers(self,data):
-        return None
+        return data.split("\r\n\r\n")[0]
 
     def get_body(self, data):
-        return None
+        return data.split("\r\n\r\n")[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,15 +70,70 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def parse_url(self, url):
+        
+        parsed_url = urllib.parse.urlparse(url)
+
+        port = parsed_url.port
+        path = parsed_url.path
+        host = parsed_url.hostname
+        port = port if port else 80
+        path = path if (path != "" and path) else "/"
+        #In the case the user doesnt enter http://
+        #then the host name is None and is contained in the path
+        if host is None:
+            slash_idx = path.find('/')
+            if slash_idx == -1:
+                host = path
+                path = "/"
+            else:
+                host = path[:slash_idx]
+                path = path[slash_idx:]
+
+        return host, path, port
+
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        host, path, port = self.parse_url(url)
+        self.connect(host, port)
+
+        request = f"GET {path} HTTP/1.1\r\n"
+        request += f"Host: {host}:{port}\r\n"
+        request += "Connection: close\r\n\r\n"
+
+        self.sendall(request)
+        response = self.recvall(self.socket)
+        self.close()
+        return HTTPResponse(self.get_code(response), self.get_body(response))
+
+    def encode_args(self,args):
+        encoded_args = ""
+        for k, v in args.items():
+            encoded_args += f"{k}={v}&"
+        encoded_args = encoded_args[:-1]
+        return encoded_args
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        host, path, port = self.parse_url(url)
+        self.connect(host, port)
+
+        request = f"POST {path} HTTP/1.1\r\n"
+        request += f"Host: {host}:{port}\r\n"
+        request += "Connection: close\r\n"
+
+        encoded_args = self.encode_args(args) if args else ""
+        print(encoded_args)
+        if args:
+            request += "Content-Type: application/x-www-form-urlencoded\r\n"
+            request += f"Content-Length: {len(encoded_args.encode('utf-8'))}\r\n\r\n"
+        else: request += "Content-Length: 0\r\n\r\n"
+
+        request += encoded_args
+
+        self.sendall(request)
+        response = self.recvall(self.socket)
+        self.close()
+
+        return HTTPResponse(self.get_code(response), self.get_body(response))
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
